524a80cd3e7a297bbc1a522fac1422d766970df8
241B013 <241B013@juet.co.in>
2026-08-03 15:53:52 +0530

    java

---
+++ Add.java
@@ -0,0 +1,9 @@
+public class Add {
+    // Method to print the table of a given number
+    public void printTable(int num) {
+        for (int i = 1; i <= 10; i++) {
+    System.out.println(num + " × " + i + " = " + (num * i));
+        }
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create an object of Add class
+        Add obj = new Add();
+
+        // Print the table of 2
+        obj.printTable(3);
+    }
+}
